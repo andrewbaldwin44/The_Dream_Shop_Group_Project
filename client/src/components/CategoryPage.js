347d405f4592990874b386d4ae62ddd9393ee65a
Matcote <matthewcotesipod@gmail.com>
2020-08-10 10:07:11 -0400

    getting items to display initially

--- client/src/components/CategoryPage.js
+++ client/src/components/CategoryPage.js
@@ -3,23 +3,39 @@ import { useDispatch, useSelector } from "react-redux";
 import { requestItems, receiveItems, receiveItemsError } from "../actions";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
-//import StoreItem from "./StoreItem";
+import StoreItem from "./StoreItem";
 
 const CategoryPage = () => {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.items.items);
-  console.log(items);
+  const categoryId = useParams().categoryId;
   React.useEffect(() => {
     dispatch(requestItems());
-    fetch()
+    fetch(`/products`)
       .then((res) => res.json())
+      .then((data) => data.items)
       .then((items) => dispatch(receiveItems(items)))
       .catch((err) => dispatch(receiveItemsError()));
     // eslint-disable-next-line
   }, []);
   return (
-    <div>{items === null ? <div>loading...</div> : <div>loaded</div>}</div>
+    <Wrapper>
+      {items === null ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          {items.map((item) => {
+            return <StoreItem item={item} key={item.id} />;
+          })}
+        </>
+      )}
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export default CategoryPage;
--- client/src/components/StoreItem.js
+++ client/src/components/StoreItem.js
@@ -1,10 +1,32 @@
-import react from "react";
+import React from "react";
 import styled from "styled-components";
 
-const StoreItem = () => {
-  return <Wrapper></Wrapper>;
+const StoreItem = ({ item }) => {
+  return (
+    <Wrapper>
+      <Image src={item.imageSrc} alt={item.name} />
+      <Price>{item.price}</Price>
+      <Name>{item.name}</Name>
+      <Button>CART</Button>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  border-radius: 8px;
+  border: 1px solid lightgrey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Price = styled.p``;
+
+const Image = styled.img``;
+
+const Button = styled.button``;
+
+const Name = styled.p``;
 
 export default StoreItem;
