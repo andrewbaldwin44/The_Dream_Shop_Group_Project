a2f859423e9d7192756a02d69152dc88bc3edf01
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-10 12:48:12 -0400

    Add all items fetch to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
-import { requestItems, receiveItems, receiveItemsError } from "../actions";
+import {
+  requestAllItemInformation,
+  receiveProducts,
+  receiveCategories,
+  receiveBrands,
+  receiveBodyLocation,
+  receiveAllItemInformationError,
+  receiveAllItemInformation,
+} from "../actions";
 
 import Homepage from "./Homepage";
 import Header from "./Header";
@@ -11,27 +19,69 @@ import CategoryPage from "./CategoryPage";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const dispatch = useDispatch();
+  const [status, setStatus] = useState("loading");
+
   useEffect(() => {
+    dispatch(requestAllItemInformation());
 
+    const products =
+      fetch(`/products`)
+        .then(response => response.json())
+        .then(data => data.products)
+        .then(products => dispatch(receiveProducts(products)))
+        .catch(error => dispatch(receiveAllItemInformationError()));
 
+    const categories =
+      fetch(`/products/categories`)
+        .then(response => response.json())
+        .then(data => data.categories)
+        .then(categories => dispatch(receiveCategories(categories)))
+        .catch(error => dispatch(receiveAllItemInformationError()));
 
+    const brands =
+      fetch(`/brands`)
+        .then(response => response.json())
+        .then(data => data.brands)
+        .then(brands => dispatch(receiveBrands(brands)))
+        .catch(error => dispatch(receiveAllItemInformationError()));
+
+    const bodyLocation =
+      fetch(`/bodylocation`)
+        .then(response => response.json())
+        .then(data => data.bodyLocation)
+        .then(bodyLocation => dispatch(receiveBodyLocation(bodyLocation)))
+        .catch(error => dispatch(receiveAllItemInformationError()));
+
+    Promise.all([products, categories, brands, bodyLocation])
+      .then(() => dispatch(receiveAllItemInformation()))
+      .then(() => setStatus("idle"));
   }, []);
 
-  return (
-    <Router>
-      <GlobalStyles />
-      <Header />
-      <Sidebar />
-      <Switch>
-        <Route exact={true} path="/">
-          <Homepage />
-        </Route>
-        <Route path="/products/:categoryId">
-          <CategoryPage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  if (status === "loading") {
+    return (
+      <div>
+          loading...
+      </div>
+    )
+  }
+  else {
+    return (
+      <Router>
+        <GlobalStyles />
+        <Header />
+        <Sidebar />
+        <Switch>
+          <Route exact={true} path="/">
+            <Homepage />
+          </Route>
+          <Route path="/products/:categoryId">
+            <CategoryPage />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/CategoryPage.js
+++ client/src/components/CategoryPage.js
@@ -1,38 +1,42 @@
 import React from "react";
+
 import { useDispatch, useSelector } from "react-redux";
-import { requestProducts, receiveProducts, receiveProductsError } from "../actions";
+
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import StoreItem from "./StoreItem";
 
 const CategoryPage = () => {
-  const dispatch = useDispatch();
-  const items = useSelector((state) => state.items.products);
-  const categoryId = useParams().categoryId;
-
-  React.useEffect(() => {
-    dispatch(requestProducts());
-    fetch(`/products/categories/${categoryId}`)
-      .then((res) => res.json())
-      .then((data) => data.productsData)
-      .then((items) => dispatch(receiveProducts(items)))
-      .catch((err) => dispatch(receiveProductsError()));
-    // eslint-disable-next-line
-  }, []);
-
   return (
-    <Wrapper>
-      {items === null ? (
-        <div>loading...</div>
-      ) : (
-        <>
-          {items.map((item) => {
-            return <StoreItem item={item} key={item.id} />;
-          })}
-        </>
-      )}
-    </Wrapper>
-  );
+    <div>...</div>
+  )
+  // const dispatch = useDispatch();
+  // const items = useSelector((state) => state.items.products);
+  // const categoryId = useParams().categoryId;
+  //
+  // React.useEffect(() => {
+  //   dispatch(requestProducts());
+  //   fetch(`/products/categories/${categoryId}`)
+  //     .then((res) => res.json())
+  //     .then((data) => data.productsData)
+  //     .then((items) => dispatch(receiveProducts(items)))
+  //     .catch((err) => dispatch(receiveProductsError()));
+  //   // eslint-disable-next-line
+  // }, []);
+  //
+  // return (
+  //   <Wrapper>
+  //     {items === null ? (
+  //       <div>loading...</div>
+  //     ) : (
+  //       <>
+  //         {items.map((item) => {
+  //           return <StoreItem item={item} key={item.id} />;
+  //         })}
+  //       </>
+  //     )}
+  //   </Wrapper>
+  // );
 };
 
 const Wrapper = styled.div`
