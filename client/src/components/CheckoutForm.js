be13d04c6659daa579a8c65b5a7df68801ff6ba0
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-11 18:44:15 -0400

    fixed errors and cleaned up pages

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { cartAddItem, cartItemAdded, cartAddItemError } from "../actions";
 
 const CheckoutForm = () => {
   return (
@@ -83,21 +81,13 @@ const SectionHeader = styled.div`
   margin-bottom: 4px;
 `;
 
-const CreditCardinfo = styled.div``;
+const CreditCardinfo = styled.div`
+  border: 1px grey solid;
+  margin-top: 2px;
+`;
 
 const Wrapper = styled.div`
   border: 2px black solid;
-`;
-const Price = styled.p`
-  font-weight: bold;
-  font-size: large;
-  color: black;
-`;
-
-const Image = styled.img`
-  height: 180px;
-
-  align-self: center;
 `;
 
 const Button = styled.button`
@@ -111,11 +101,6 @@ const Button = styled.button`
     cursor: pointer;
     background-color: #5f6368;
   }
-`;
-
-const Name = styled.p`
-  margin: 10px 0;
-  color: #5f6368;
 `;
 
 export default CheckoutForm;
--- client/src/components/CheckoutPage.js
+++ client/src/components/CheckoutPage.js
@@ -7,9 +7,9 @@ import CheckoutForm from "./CheckoutForm";
 const CheckoutPage = () => {
   const cartData = useSelector((state) => state.cart.cart);
   let total = 0;
-  cartData.map((item) => (
-    total += parseFloat(item.price.replace("$", "")) * item.quantity
-  ));
+  cartData.map(
+    (item) => (total += parseFloat(item.price.replace("$", "")) * item.quantity)
+  );
   return (
     <Wrapper>
       <Back>Return to Cart:</Back>
@@ -18,7 +18,7 @@ const CheckoutPage = () => {
         {cartData.map((data) => (
           <CheckoutItem item={data} key={data.id} />
         ))}
-        <div>TOTAL : ${total}</div>
+        <div>ORDER TOTAL : ${total}</div>
       </ItemsBox>
       <CheckoutForm></CheckoutForm>
     </Wrapper>
@@ -27,7 +27,6 @@ const CheckoutPage = () => {
 
 const Wrapper = styled.div``;
 const ItemsBox = styled.div`
-  border: 2px yellow solid;
   width: 70vw;
   padding: 10px;
 `;
@@ -36,10 +35,5 @@ const Back = styled.div`
   font-size: large;
   margin-bottom: 5vh;
 `;
-const Text = styled.p`
-  position: relative;
-  bottom: 300px;
-  color: white;
-  font-size: x-large;
-`;
+
 export default CheckoutPage;
