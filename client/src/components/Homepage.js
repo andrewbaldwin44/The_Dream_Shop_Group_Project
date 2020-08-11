3d45a62d6d6222e19684f1fa616bfdba9bef582f
Stephanos Criticos <scritico@unb.ca>
2020-08-11 11:43:05 -0400

    home changes

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import light from "../assets/light.jpeg";
 const Homepage = () => {
   return (
     <Wrapper>
-      <Banner src="../assets/fitness.jpg" />
-      <Text>Fitness</Text>
+      <Banner src={light} />
+      <Text>
+        Welcome to Dreamshop. Your place to experience dreams. Browse our
+        products to fulfill all your dreams
+      </Text>
+      <Shoplink to="/brands">Shop now</Shoplink>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div``;
 const Banner = styled.img`
-  height: 500px;
-  width: 100vw;
+  height: 300px;
+  width: 50vw;
+  border-radius: 5px;
   object-fit: cover;
   overflow: hidden;
 `;
@@ -22,7 +28,13 @@ const Text = styled.p`
   bottom: 400px;
   left: 30px;
   color: white;
-  font-size: 40px;
-  font-weight: bold;
+  text-align: center;
+  font-size: 20px;
+  font-style: italic;
+  width: 30vw;
+`;
+const Shoplink = styled(Link)`
+  border: 1 px solid black;
+  bottom: 320px;
 `;
 export default Homepage;
