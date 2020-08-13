53dbe35dc86861501037dfc9e6b60322ce40ed24
Matcote <matthewcotesipod@gmail.com>
2020-08-13 11:55:52 -0400

    message for empty cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -19,9 +19,15 @@ const Cart = () => {
       <h2>Shopping Cart</h2>
       <CartWrapper>
         <CartHeader />
-        {cart.map((item, index) => {
-          return <CartItem item={item} index={index} key={item.id} />;
-        })}
+        {cart.length > 0 ? (
+          cart.map((item, index) => {
+            return <CartItem item={item} index={index} key={item.id} />;
+          })
+        ) : (
+          <Message>
+            <span>Psst... You forgot to add something to your cart...</span>
+          </Message>
+        )}
         <CartFooter />
         <Button
           onClick={() => history.push("/checkout")}
@@ -79,5 +85,14 @@ const Button = styled.button`
       border: none;
     }
   }
+`;
+
+const Message = styled.div`
+  width: 1100px;
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: xx-large;
 `;
 export default Cart;
