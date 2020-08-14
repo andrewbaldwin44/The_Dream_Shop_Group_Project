775eb05715c67fcb54cc52849f487cfa275d04a7
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-13 21:12:00 -0400

    functional confirm page

--- client/src/components/ConfirmPage.js
+++ client/src/components/ConfirmPage.js
@@ -1,31 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import CheckoutItem from "./CheckoutItem";
-import CheckoutForm from "./CheckoutForm";
+
 import ConfirmItem from "./ConfirmItem.js";
-import { Link } from "react-router-dom";
-import { GrFormPreviousLink } from "react-icons/gr";
 
 const ConfirmPage = () => {
-  const confirmationData = useSelector(
-    (state) => state.order.order.itemsPurchased
-  );
-  console.log("**********PAGEPAGE", confirmationData);
-  const confirmationNumber = useSelector((state) => state.order.order.orderNo);
+  const initialize = useSelector((state) => state.order.order);
 
-  let itemGeneration =
-    confirmationData === undefined || null
-      ? null
-      : confirmationData.map((item) => <ConfirmItem data={item} />);
-
-  return (
-    <div>
-      <div>Thank you for your order!</div>
-      <div>Confirmation number: {confirmationNumber}</div>
-      <div>{itemGeneration}</div>{" "}
-    </div>
-  );
+  // const confirmationData = useSelector(
+  //   (state) => state.order.order.itemsPurchased
+  // );
+  if (!initialize) {
+    return <div>There's nothing in your cart!</div>;
+  } else {
+    const { itemsPurchased } = initialize;
+    return (
+      <>
+        <div>Your Order: {initialize.orderNo}</div>
+        <ConfirmItem data={itemsPurchased} />
+        <div> Thank you for your business! </div>
+      </>
+    );
+  }
 };
 
 export default ConfirmPage;
